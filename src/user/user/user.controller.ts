b0ae6041e0c2d252b38a37ac80df9083f3b2578d
hilmiyahya19 <hilmiyahya199@gmail.com>
2025-11-17 12:00:06 +0700

    HTTP Request & HTTP Response

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,7 +1,59 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  Param,
+  Post,
+  Query,
+  Redirect,
+} from '@nestjs/common';
+import type { HttpRedirectResponse } from '@nestjs/common';
 
 @Controller('/api/users')
 export class UserController {
+  // Http Response menggunakan decorator @header untuk menambahkan header, @HttpCode untuk menambahkan http status code
+  @Get('/sample-response')
+  @Header('Content-Type', 'application/json')
+  @HttpCode(200)
+  sampleResponse(): Record<string, string> {
+    return {
+      data: 'Hello JSON',
+    };
+  }
+
+  // Http Response menggunakan decorator @Redirect (static)
+  @Get('/redirect-static')
+  @Redirect('/api/users/sample-response', 301)
+  redirectStatic() {}
+
+  // Http Response menggunakan decorator @Redirect (dynamic)
+  @Get('/redirect-dynamic')
+  @Redirect()
+  redirectDynamic(): HttpRedirectResponse {
+    const shouldGoToSample = true; // contoh kondisi dinamis
+
+    return {
+      url: shouldGoToSample ? '/api/users/sample-response' : '/api/users/hello',
+      statusCode: 302,
+    };
+  }
+
+  // Http Request Query menggunakan decorator @Query
+  @Get('/hello')
+  sayHello(
+    @Query('first_name') firstName: string,
+    @Query('last_name') lastName: string,
+  ): string {
+    return `Hello ${firstName} ${lastName}`;
+  }
+
+  // Http Request Param mennggunakan decorator @Param
+  @Get('/:id')
+  getById(@Param('id') id: string): string {
+    return `GET ${id}`;
+  }
+
   @Post()
   post(): string {
     return 'POST';
